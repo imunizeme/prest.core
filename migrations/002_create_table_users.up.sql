5779288816872e30be947e2580f84b8b0968c792
Felipe Oliveira <felipeweb.programador@gmail.com>
2017-07-08 11:23:35 -0300

    unique key

--- migrations/002_create_table_users.up.sql
+++ migrations/002_create_table_users.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id serial NOT NULL,
     name character varying(255) NOT NULL,
     password character varying(255) NOT NULL,
-    cpf character varying(15) NOT NULL,
+    cpf character varying(15) UNIQUE NOT NULL,
     created_at timestamp without time zone DEFAULT now(),
     last_update timestamp without time zone DEFAULT now(),
     PRIMARY KEY (id)
