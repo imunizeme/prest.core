300833eacdcefd376bad87887da97f753ba331c9
Décio Montanhani <deciomontanhani@gmail.com>
2017-07-30 20:35:13 -0300

    Criando tabela clinicas e address (#5)
    * Criando tabela clinicas e address
    
    * Alterando tabela users com cnpj

--- migrations/006_create_table_clinicas.up.sql
+++ migrations/006_create_table_clinicas.up.sql
@@ -1,12 +1,12 @@
-CREATE TABLE users (
+CREATE TABLE clinicas (
     id serial NOT NULL,
-    password character varying(255) NOT NULL,
-    cpf_cnpj character varying(20) UNIQUE NOT NULL,
+    name character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
     created_at timestamp without time zone DEFAULT now(),
     last_update timestamp without time zone DEFAULT now(),
     PRIMARY KEY (id)
 );
 
-CREATE TRIGGER update_users
-BEFORE UPDATE ON users
+CREATE TRIGGER update_clinicas
+BEFORE UPDATE ON clinicas
 FOR EACH ROW EXECUTE PROCEDURE update_datetime();
