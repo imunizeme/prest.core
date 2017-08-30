5b902ec9faafbb4651dc6e12a3f0c4ca9840e628
Décio Montanhani <deciomontanhani@gmail.com>
2017-08-30 07:43:28 -0300

    Adicionando FK na carteirinha

--- migrations/012_create_table_carteirinha.up.sql
+++ migrations/012_create_table_carteirinha.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE carteirinha(
     id serial NOT NULL,
     vacinas_id serial REFERENCES vacinas (id),
     ative boolean NOT NULL,
+    user_id serial REFERENCES users (id),
     created_at timestamp without time zone DEFAULT now(),
     last_update timestamp without time zone DEFAULT now(),
     PRIMARY KEY (id)
