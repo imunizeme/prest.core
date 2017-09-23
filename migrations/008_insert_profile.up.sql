b51db97171150dce12061f21bc71f92691b6070f
Décio Montanhani <deciomontanhani@gmail.com>
2017-09-23 18:55:49 -0300

    ajustes clinicas e profile

--- migrations/008_insert_profile.up.sql
+++ migrations/008_insert_profile.up.sql
@@ -1,2 +1,2 @@
-INSERT INTO profile (name, email, birth_date, user_id) 
-VALUES ('Décio Lucas Borges Montanhani', 'deciomontanhani@gmail.com', '1996-08-27', '1');
+INSERT INTO profile (name, email, birth_date, user_id, sexo) 
+VALUES ('Décio Lucas Borges Montanhani', 'deciomontanhani@gmail.com', '1996-08-27', '1', 'M');
