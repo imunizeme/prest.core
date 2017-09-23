87ea02086e4a498bbf21781c3a0e4efa47f7fa95
Felipe Oliveira <felipeweb.programador@gmail.com>
2017-09-23 16:49:39 -0300

    adicionando mais um grupo (#9)

--- migrations/010_insert_ref_group.up.sql
+++ migrations/010_insert_ref_group.up.sql
@@ -3,3 +3,4 @@ INSERT INTO ref_group (name) VALUES ('6 meses até 12 meses');
 INSERT INTO ref_group (name) VALUES ('15 meses até 10 anos');
 INSERT INTO ref_group (name) VALUES ('11 anos até 19 anos');
 INSERT INTO ref_group (name) VALUES ('20 anos até 59 anos');
+INSERT INTO ref_group (name) VALUES ('60 anos ou mais');
