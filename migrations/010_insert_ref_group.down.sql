23be42f14f97ca931ff3d676a581951fdd16788c
Décio Montanhani <deciomontanhani@gmail.com>
2017-09-23 16:44:08 -0300

    adicionando mais um grupo

--- migrations/010_insert_ref_group.down.sql
+++ migrations/010_insert_ref_group.down.sql
@@ -3,3 +3,4 @@ DELETE FROM ref_group WHERE name = '6 meses até 12 meses';
 DELETE FROM ref_group WHERE name = '15 meses até 10 anos';
 DELETE FROM ref_group WHERE name = '11 anos até 19 anos';
 DELETE FROM ref_group WHERE name = '20 anos até 59 anos';
+DELETE FROM ref_group WHERE name = '60 anos ou mais';
