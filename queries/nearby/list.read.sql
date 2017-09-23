b988f5d4e3d2c8bb4166775df3effafc0fbbcb15
Felipe Oliveira <felipeweb.programador@gmail.com>
2017-09-23 20:24:57 -0300

    add patrocinado on query

--- queries/nearby/list.read.sql
+++ queries/nearby/list.read.sql
@@ -1,3 +1,3 @@
-SELECT c.nome_clinica, a.*  
+SELECT c.nome_clinica, c.patrocinado, a.*  
 FROM clinicas c INNER JOIN address a ON c.address_id = a.id 
 ORDER BY POINT({{.lat}}, {{.lng}}) <-> a.local LIMIT 10
