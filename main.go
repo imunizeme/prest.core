946d1a3cd14ef76399f70b65f73bba64444c8146
Felipe Oliveira <felipeweb.programador@gmail.com>
2017-09-24 21:55:57 -0300

    fix cors

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	config "github.com/imunizeme/config.core"
 	"github.com/nuveo/log"
 	"github.com/prest/cmd"
+	"github.com/prest/middlewares"
+	"github.com/rs/cors"
 )
 
 func main() {
 	if err := config.Load(); err != nil {
 		log.Fatal("Unable to load config", err.Error())
 	}
+	app := middlewares.GetApp()
+	app.Use(cors.Default())
 	cmd.Execute()
 }
