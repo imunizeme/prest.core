2e819c26208bc081804c17b3fad453b0080bde42
Felipe Oliveira <felipeweb.programador@gmail.com>
2017-09-26 01:53:13 -0300

    report

---
+++ queries/vacinas/report.read.sql
@@ -0,0 +1,14 @@
+SELECT va.nome_vacina, p.sexo, p.birth_date, cv.data_tomada, COUNT(cv.id) AS vacinas_tomadas FROM vacinas va 
+INNER JOIN carteirinha_vacina cv ON va.id = cv.vacinas_id 
+INNER JOIN profile p ON p.user_id = cv.user_id 
+WHERE 1 = 1 
+{{if isSet "sexo"}}
+AND p.sexo = '{{.sexo}}' 
+{{end}}
+{{if isSet "birth"}}
+AND p.birth_date > '{{.birth}}' 
+{{end}}
+{{if isSet "tomada"}}
+AND cv.data_tomada = '{{.tomada}}' 
+{{end}}
+GROUP BY va.nome_vacina, p.sexo, p.birth_date, cv.data_tomada
